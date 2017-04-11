ec44f7a9fb42bb91c60789a159bb5c55cb99cce5
Alex Marse <alexmarse@Alexs-MacBook-Pro-3.local>
2017-04-11 09:18:07 -0700

    Take inventory into account

--- app.py
+++ app.py
@@ -55,27 +55,48 @@ for order in order_results['orders']:
 		if 'Sub' in sku or 'Staff' in sku:
 			sku = 'Sub' + '-' + sku.split('-')[1]
 
-		# Init the sku coffees dict the first time
+		# Init the sku coffees dict entry for each new sku
 		if not coffees.get(sku):
 			coffees[sku] = 0
 		
 		# Increment the coffee sku entry by the quantity in the current order
 		coffees[sku] += quantity
 
-# Init the new rows list, which will store the quantities for each sku
-new_rows_list = []
-
 # Get the user's home dir from the shell
 home_dir = os.getenv('HOME')
 roast_dir = home_dir + '/Dropbox/Cat & Cloud Admin/Roasting'
 
-# Open up and read the csv file template
-with open(roast_dir + '/Roast Day Template.csv', 'r') as csvfile:
-	
+# Open up the previous roast day csv file, copy EOD Stock
+prev_roast_file = None
+curr_stock = {}
+for file in os.listdir(roast_dir):
+	if file.endswith("Roast.csv"):
+		prev_roast_file = roast_dir + '/' + file
+
+if prev_roast_file:
+	with open(prev_roast_file, 'r') as csvfile:
+		reader = csv.DictReader(csvfile)
+
+		# Loop through rows of csv file
+		for row in reader:
+			curr_sku = row.get('SKU')
+			
+			if not curr_stock.get(curr_sku):
+				curr_stock[curr_sku] = 0
+			
+			if row.get('EOD STOCK'):
+				curr_stock[curr_sku] = row.get('EOD STOCK')
+else:
+	print "Could not find previous roast day file - input current stock manually"
+
+# Init the new rows list, which will store the quantities for each sku
+new_rows_list = []
+
+with open(roast_dir + '/Roast Day Template New.csv', 'r') as csvfile:	
 	reader = csv.DictReader(csvfile)
 
 	# Loop through rows of csv file
-	for row in reader:
+	for idx, row in enumerate(reader):
 		if row.get('SKU'):
 			# Loop through coffees and write to the proper row/column, according to sku
 			for coffee, quantity in coffees.iteritems():
@@ -84,13 +105,16 @@ with open(roast_dir + '/Roast Day Template.csv', 'r') as csvfile:
 
 				if row.get('SKU') == sku_cfe:
 					row[sku_size] = quantity
+
+					if curr_stock.get(sku_cfe):
+						row['IN STOCK'] = curr_stock[sku_cfe]
 		new_rows_list.append(row)
 
 # Write to a new csv file
-roast_date_file = '%s_Roast.csv' % arrow.utcnow().to('US/Pacific').format('YYYY-MM-DD')
-write_file = roast_dir + '/' + roast_date_file
+curr_roast_file = '%s_Roast.csv' % arrow.utcnow().to('US/Pacific').format('YYYY-MM-DD')
+write_file = roast_dir + '/' + curr_roast_file
 with open(write_file, 'w') as csvfile:
-	fieldnames = ['SKU', 'Coffee', '10oz', '1lb', 'KILO', '5lb', 'ROASTED LBS', 'GREEN LBS']
+	fieldnames = ['SKU', 'Coffee', '10oz', '1lb', 'KILO', '5lb', 'IN STOCK', 'ROASTED LBS', 'GREEN LBS', 'BATCHES', 'EOD STOCK']
 	writer = csv.DictWriter(csvfile, fieldnames)
 	
 	writer.writeheader()
@@ -101,6 +125,6 @@ archive_dir_path = roast_dir + '/Archive/Roast Day Archives'
 for file in os.listdir(roast_dir):
 	if file.endswith("Roast.csv"):
 		# Skip the current roast file
-		if file == roast_date_file:
+		if file == curr_roast_file:
 			continue
 		shutil.move(roast_dir + '/' + file, archive_dir_path + '/' + file)
